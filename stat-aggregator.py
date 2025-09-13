b06e6df70387f7c66f59fe4f6e0fd0f78070d321
Deathstar8 <deathstar8@outlook.com>
2025-09-13 16:13:13 -0500

    Aggregator fully complete; also output to .csv functionality

--- stat-aggregator.py
+++ stat-aggregator.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import json
 import requests
+import itertools
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 site_html = requests.get('https://overwatch.blizzard.com/en-us/rates/').text
 soup = BeautifulSoup(site_html, 'lxml')
@@ -11,6 +13,8 @@ param = soup.find_all(value=True) # All parameters for hero stats
 def fetch_values(value_list, output_list, label, search_attr):
   for i in value_list:
     if i.parent.has_attr(search_attr) and i.parent[search_attr] == label:
+      output_list.append(i['value'])
+    elif i.parent.parent.has_attr(search_attr) and i.parent.parent[search_attr] == label: #For fetching map values
       output_list.append(i['value'])
 
 input_values = []
@@ -26,28 +30,36 @@ fetch_values(param, input_values, "input", 'data-label')
 fetch_values(param, rq_values, "rq", 'data-label')
 fetch_values(param, tier_values, "tier", 'data-label')
 fetch_values(param, region_values, "region", 'data-label')
+fetch_values(param, map_values, "map", 'data-label')
 
-#Fetch all map_values (they get a different one due to the html structure for the map dropdown)
-for i in param:
-  if i.parent.has_attr('data-label') and i.parent['data-label'] == "map": #Account for 'all-maps'
-    map_values.append(i['value'])
-  elif i.parent.parent.has_attr('data-label') and i.parent.parent['data-label'] == "map":
-    map_values.append(i['value'])
-
-#Fetch titles for game modes (just in case!)
+#Fetch titles for game modes (just in case!) and put them in a dictionary
 for i in param:
   if i.parent.has_attr('data-label') and i.parent['data-label'] == "rq":
     rq_dict.update({i['value'] : i['data-title']})
 
-params = {"input" : input_values[0], "map" : map_values[0], "region" : region_values[0], "role" : "All", "rq" : rq_values[1], "tier" : tier_values[0]}
+input_combos = list(itertools.product(input_values, rq_values, tier_values, region_values, map_values))
 
-#Change tier to "All" if Quick Play
-if params["rq"] == "0":
-  params.update({"tier" : "All"})
+def get_data():
+  params = {"input" : input_values[1], "map" : map_values[0], "region" : region_values[2], "role" : "All", "rq" : rq_values[1], "tier" : tier_values[3]}
 
-data = json.loads(requests.get("https://overwatch.blizzard.com/en-us/rates/data/", params=params).text)
+  data_request = requests.get("https://overwatch.blizzard.com/en-us/rates/data/", params=params)
+  if data_request.status_code != 200 and data_request.status_code == 404:
+    print("Error! Data not found!")
+  elif data_request.status_code != 200:
+    print("Warning, encountered unexpected status code: " + data_request.status_code)
 
-df = pd.DataFrame(pd.json_normalize(data["rates"])).drop(columns=["id", "hero.portrait", "hero.name", "hero.color", "hero.roleIcon"]).rename(columns={"cells.name" : "Hero", "cells.pickrate" : "Pick rate", "cells.winrate" : "Win rate", "hero.role" : "Role"})
-df[["Input", "Game Mode", "Tier", "Map", "Region"]] = [params["input"], rq_dict[params["rq"]], params["tier"], params["map"], params["region"]]
+  data = json.loads(data_request.text)
 
-print(df)
+  #Check if loaded data params matches input params
+  #If they don't match, it probably means the input params were invalid (like fetching winrates based on competitive rank, but selecting Quick Play)
+  if data['selected'] != params:
+    print("Warning: Fetched data has different parameters than input parameters. Fetched data is most likely not reflective of input parameters.")
+    print("Input parameters: " + str(params))
+    print("Recieved (selected by website) parameters: " + str(data['selected']))
+
+  df = pd.DataFrame(pd.json_normalize(data["rates"])).drop(columns=["id", "hero.portrait", "hero.name", "hero.color", "hero.roleIcon"]).rename(columns={"cells.name" : "Hero", "cells.pickrate" : "Pick rate", "cells.winrate" : "Win rate", "hero.role" : "Role"})
+  df[["Input", "Game Mode", "Tier", "Map", "Region", "Timestamp"]] = [data['selected']["input"], rq_dict[data['selected']["rq"]], data['selected']["tier"], data['selected']["map"], data['selected']["region"], datetime.now().astimezone().strftime("%d-%m-%Y %H:%M:%S UTC%z")]
+  return df
+
+data = get_data()
+data.to_csv('output.csv', index=False)
